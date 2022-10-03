840209ed76109ae475f6de01194d69db5425bf49
Maurice Parker <mo@vincode.io>
2022-10-03 04:37:17 -0500

    Added a segmented control to the toolbar

--- ToolbarSegmentedControl/ToolbarSegmentedControlApp.swift
+++ ToolbarSegmentedControl/ToolbarSegmentedControlApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct ToolbarSegmentedControlApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	
+	private let items = ["About", "Credits"]
+	@State private var selected = 0
+	
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+				.toolbar {
+					ToolbarItem(placement: .principal) {
+						Picker("", selection: $selected) {
+							ForEach(items, id: \.self) { item in
+								Text(item)
+							}
+						}
+						.pickerStyle(.segmented)
+					}
+				}
+				.frame(minWidth: 400, minHeight: 400)
+		}
+		.windowToolbarStyle(.unifiedCompact(showsTitle: false))
+	}
 }
