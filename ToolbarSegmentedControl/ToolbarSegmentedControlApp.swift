c9022d46f1df5ea271d46ff460373cf5a81ad9df
Maurice Parker <mo@vincode.io>
2022-10-03 04:16:04 -0500

    Initial Commit

---
+++ ToolbarSegmentedControl/ToolbarSegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarSegmentedControlApp.swift
+//  ToolbarSegmentedControl
+//
+//  Created by Maurice Parker on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarSegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
